ede1d98ae275d8fb79a41ccb06038347e6b862cd
NarminSH <nshivakhanova@gmail.com>
2022-09-06 20:09:02 +0400

    latest

--- GenerateTemplate/Program.cs
+++ GenerateTemplate/Program.cs
@@ -3,15 +3,16 @@ using System.IO;
 using System.Data.SqlClient;
 using System.Collections.Generic;
 
+
 namespace GenerateTemplate
 {
     class Program
     {
         static void Main(string[] args)
         {
-            const string connection = "server=localhost;database=Northwind;Trusted_Connection=True";
+            const string connection = "server=localhost;database=Northwind;Trusted_Connection=True;MultipleActiveResultSets=True";
             SqlConnection con = new SqlConnection(connection);                                                                                       
-            SqlCommand cmd = new SqlCommand("select table_name from INFORMATION_SCHEMA.TABLES ", con);
+            SqlCommand cmd = new SqlCommand("select table_name from INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE = 'BASE TABLE'", con);
             if (con.State == System.Data.ConnectionState.Closed)
                 con.Open();
 
@@ -19,15 +20,14 @@ namespace GenerateTemplate
             SqlDataReader dr = cmd.ExecuteReader();
             while (dr.Read())
             {
+                
                 tableNames.Add(dr[0]);
             }
 
             dr.Close();
-            con.Close();
 
             string pathString = @"C:\Users\narmin.psh\source\repos\GenerateTemplate\GenerateTemplate\NewFolder\myDir1";
             DirectoryInfo di = new DirectoryInfo(pathString);
-            
             if (di.Exists)
             {
                 Console.WriteLine("That path exists already.");
@@ -37,38 +37,49 @@ namespace GenerateTemplate
                 di.Create();
                 Console.WriteLine("The directory was created successfully.");
             }
+   
             foreach (var item in tableNames)
             {
-
-                string path = System.IO.Path.Combine(pathString, $"{item}.cs");
-                Console.WriteLine("Path to my file: {0}\n", path);
-                if (!System.IO.File.Exists(path))
+                string path = Path.Combine(pathString, $"{item}.cs");
+                if (!File.Exists(path))
                 {
-                    System.IO.File.Create(path);
+                    File.Create(path);
                     path = @"C:\Users\narmin.psh\source\repos\GenerateTemplate\GenerateTemplate\NewFolder\myDir1";
-
                 }
                 else
                 {
                     Console.WriteLine("File \"{0}\" already exists.", item);
                 }
-            }
-
-            List<object> columnNames = new List<object>();
-            if (con.State == System.Data.ConnectionState.Closed)
-                con.Open();
-            foreach (var name in tableNames)
-            {
-               SqlCommand cmd1 = new SqlCommand($"SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME LIKE \'{name}\'", con);
-               SqlDataReader dr1 = cmd1.ExecuteReader();
+    
+                SqlCommand cmd1 = new SqlCommand($"SELECT COLUMN_NAME, DATA_TYPE FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME LIKE \'{item}\'", con);
+                SqlDataReader dr1 = cmd1.ExecuteReader();
+                string examplePath1 = $@"C:\Users\narmin.psh\source\repos\GenerateTemplate\GenerateTemplate\NewFolder\myDir1\{item}.cs";
+                StreamWriter sw = File.CreateText(examplePath1);
+                sw.WriteLine($"public class {item} ");
+                sw.WriteLine("{\n");
                 while (dr1.Read())
                 {
-                    columnNames.Add(dr1[0]);    
-                    Console.WriteLine(dr1[0]);
+                    string type = "";
+                    if (dr1[1].ToString() == "nvarchar")
+                    {
+                        type = "string";
+                    }
+                    else if (dr1[1].ToString() == "int" || dr1[1].ToString() == "smallint" || dr1[1].ToString() == "bit" || dr1[1].ToString() == "money")
+                    {
+                        type = "int";
+                    }
+                    sw.WriteLine($"public {type} {dr1[0]}; \n");
+                    sw.Close();
                 }
                 dr1.Close();
-                con.Close();
+                sw.WriteLine("\n}");
+                sw.Close();
+                Console.WriteLine("Path to my file: {0}\n", path);
             }
+            con.Close();
         }
+
+
     }
-}
+    }
+
