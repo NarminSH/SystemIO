aa3c356ddb41485c89eff2a2d8c3e5a21ae8c3a5
NarminSH <nshivakhanova@gmail.com>
2022-09-06 20:25:31 +0400

    add tt* file

--- GenerateTemplate/Program.cs
+++ GenerateTemplate/Program.cs
@@ -16,31 +16,15 @@ namespace GenerateTemplate
             if (con.State == System.Data.ConnectionState.Closed)
                 con.Open();
 
-            //List<object> tableNames = new List<object>();
             SqlDataReader dr = cmd.ExecuteReader();
             while (dr.Read())
             {
                 string pathString = @"C:\Users\narmin.psh\source\repos\GenerateTemplate\GenerateTemplate\NewFolder\myDir1";
                 DirectoryInfo di = new DirectoryInfo(pathString);
-                if (di.Exists)
-                {
-                    Console.WriteLine("That path exists already.");
-                }
-                else
+                if (!di.Exists)
                 {
                     di.Create();
-                    Console.WriteLine("The directory was created successfully.");
                 }
-                //string path = Path.Combine(pathString, $"{dr[0]}.cs");
-                //if (!File.Exists(path))
-                //{
-                //    File.Create(path);
-                //    path = @"C:\Users\narmin.psh\source\repos\GenerateTemplate\GenerateTemplate\NewFolder\myDir1";
-                //}
-                //else
-                //{
-                //    Console.WriteLine("File \"{0}\" already exists.", dr[0]);
-                //}
 
                 SqlCommand cmd1 = new SqlCommand($"SELECT COLUMN_NAME, DATA_TYPE FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME LIKE \'{dr[0]}\'", con);
                 SqlDataReader dr1 = cmd1.ExecuteReader();
@@ -67,51 +51,7 @@ namespace GenerateTemplate
             }
             dr.Close();
             con.Close();
-
-
-
-            //foreach (var item in tableNames)
-            //{
-            //    string path = Path.Combine(pathString, $"{item}.cs");
-            //    if (!File.Exists(path))
-            //    {
-            //        File.Create(path);
-            //        path = @"C:\Users\narmin.psh\source\repos\GenerateTemplate\GenerateTemplate\NewFolder\myDir1";
-            //    }
-            //    else
-            //    {
-            //        Console.WriteLine("File \"{0}\" already exists.", item);
-            //    }
-
-            //    SqlCommand cmd1 = new SqlCommand($"SELECT COLUMN_NAME, DATA_TYPE FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME LIKE \'{item}\'", con);
-            //    SqlDataReader dr1 = cmd1.ExecuteReader();
-            //    string examplePath1 = $@"C:\Users\narmin.psh\source\repos\GenerateTemplate\GenerateTemplate\NewFolder\myDir1\{item}.cs";
-            //    StreamWriter sw = File.CreateText(examplePath1);
-            //    sw.WriteLine($"public class {item} ");
-            //    sw.WriteLine("{\n");
-            //    while (dr1.Read())
-            //    {
-            //        string type = "";
-            //        if (dr1[1].ToString() == "nvarchar")
-            //        {
-            //            type = "string";
-            //        }
-            //        else if (dr1[1].ToString() == "int" || dr1[1].ToString() == "smallint" || dr1[1].ToString() == "bit" || dr1[1].ToString() == "money")
-            //        {
-            //            type = "int";
-            //        }
-            //        sw.WriteLine($"public {type} {dr1[0]}; \n");
-            //        sw.Close();
-            //    }
-            //    dr1.Close();
-            //    sw.WriteLine("\n}");
-            //    sw.Close();
-            //    Console.WriteLine("Path to my file: {0}\n", path);
-            //}
-            con.Close();
         }
-
-
     }
     }
 
